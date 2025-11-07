8387791c1c6724385825c402a5d8942cf6184ef1
huynd653 <duchuy653201@gmail.com>
2025-11-07 17:22:59 +0700

    update themeprovider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { AppConfig } from '@/config/app.config';
+import { ThemeProvider } from 'next-themes';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -35,7 +36,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem> {children}</ThemeProvider>
       </body>
     </html>
   );
