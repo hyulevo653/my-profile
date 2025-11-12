11fc883b857d4dad8e4ca1f3102f15be55e939a4
huynd653 <duchuy653201@gmail.com>
2025-11-12 14:14:13 +0700

    change title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,8 @@ const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
+  title: "Hyulevo blog :D",
+  description: "Created with nextjs to learn",
   generator: "v0.app",
   icons: {
     icon: [
